bd318dc60edd034a06dc95a48a33303d56ffcf1f
yousung1020 <choiyousung1020@gmail.com>
2023-03-08 00:55:12 +0900

    add files

---
+++ src/chap_01/_06_Constants.java
@@ -0,0 +1,11 @@
+package chap_01;
+
+public class _06_Constants {
+    public static void main(String[] args) {
+        final String KR_COUNTRY_CODE = "+82"; // 국가 번호
+        // KR_COUNTRY_CODE = "+8282"; // 상수는 변경 불가능
+        System.out.println(KR_COUNTRY_CODE);
+
+        final double PI = 3.141582;
+    }
+}
---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,14 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busInfo = "상암08";
+        int minute = 3;
+        float distance = 1.2f;
+        System.out.println(busInfo + "번 버스");
+        System.out.println("약 " + minute + "분 후 도착");
+        System.out.println("남은 거리 " + distance + "km");
+
+        // 2:01:30 (산술 연산자 (전반전) 부터)
+    }
+}
