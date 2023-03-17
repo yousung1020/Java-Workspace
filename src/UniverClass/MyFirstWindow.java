2b7aed75dfea6ba3182e43cd94baa2e443036b4d
yousung1020 <choiyousung1020@gmail.com>
2023-03-17 11:32:57 +0900

    Add MyFirstWindow file

---
+++ src/UniverClass/MyFirstWindow.java
@@ -0,0 +1,12 @@
+package UniverClass;
+
+import javax.swing.*;
+
+public class MyFirstWindow {
+    public static void main(String[] args) {
+        JFrame myFirstWindow = new JFrame();
+        myFirstWindow.setSize(800, 600);
+        myFirstWindow.setVisible(true);
+    }
+}
+
